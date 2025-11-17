942161b5e1092e794659eb678905a4a66df6abd9
Mission127 <mission127outreach@gmail.com>
2025-11-17 17:19:12 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e293b",
+        accent: "#dc2626"
+      }
+    }
+  },
+  plugins: []
+}
