074043bc63137f06d8e91d8ef1d1aec40e52c188
Mission127 <mission127outreach@gmail.com>
2025-11-17 21:51:58 -0500

    Update logo.js

--- app/logo.js
+++ app/logo.js
@@ -1,10 +1,10 @@
 export default function Logo() {
   return (
-    <div className="w-32 h-32 mx-auto">
+    <div className="w-48 h-48 mx-auto mb-4">
       <img 
         src="https://ucarecdn.com/2f8f4b8f-9c35-4e1b-9c4f-5d8e8f8e8f8e/-/format/auto/-/quality/smart/mission127-logo-white.png" 
-        alt="Mission 1:27"
-        className="w-full h-full object-contain"
+        alt="Mission 1:27" 
+        className="w-full h-full object-contain" 
       />
     </div>
   );
