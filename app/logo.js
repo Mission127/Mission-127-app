b8b97d32a8d27f809ef2de024d1d1663e3a583bb
Mission127 <mission127outreach@gmail.com>
2025-11-17 21:27:51 -0500

    Create logo.js

---
+++ app/logo.js
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="w-32 h-32 mx-auto">
+      <img src="https://i.imgur.com/5vJjYpP.png" alt="Mission 127" className="w-full h-full object-contain" />
+    </div>
+  );
+}
